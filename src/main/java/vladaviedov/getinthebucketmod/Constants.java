52c800cbcf6cd8d9084868e24afbf68969c98afc
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-05-26 22:42:40 -0400

    kinda working version

--- src/main/java/vladaviedov/getinthebucketmod/Constants.java
+++ src/main/java/vladaviedov/getinthebucketmod/Constants.java
@@ -5,8 +5,10 @@ import net.minecraft.world.item.CreativeModeTab;
 public class Constants {
 
 	public static final String MOD_ID = "getinthebucketmod";
+	public static final CreativeModeTab CREATIVE_TAB = CreativeModeTab.TAB_MISC;
+
+	/* NBT */
 	public static final String DATA_TAG = "EntityData";
 	public static final String UUID_TAG = "uuid";
-	public static final CreativeModeTab CREATIVE_TAB = CreativeModeTab.TAB_MISC;
 
 }
