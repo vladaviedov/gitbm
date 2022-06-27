9cbe1f1226568c046d55bbc4cdd87713c63a2d2a
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-27 13:28:34 -0400

    minor documentation changes

--- src/main/java/vladaviedov/getinthebucketmod/Main.java
+++ src/main/java/vladaviedov/getinthebucketmod/Main.java
@@ -4,11 +4,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
-public class Main
-{
+public class Main {
 
-	public Main()
-	{
+	public Main() {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
