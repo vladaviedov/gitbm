7a0fbf2cfe2136b1c8f570856723a192d1fe911e
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-06-27 11:04:30 -0400

    updated to 1.19 + deferred registries

--- src/main/java/vladaviedov/getinthebucketmod/Main.java
+++ src/main/java/vladaviedov/getinthebucketmod/Main.java
@@ -4,11 +4,10 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
-public class Main
-{
+public class Main {
 
-	public Main()
-	{
+	public Main() {
+		Registry.registerItems();
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
